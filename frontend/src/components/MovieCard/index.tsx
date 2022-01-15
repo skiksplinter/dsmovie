89b73443ec04247673f8f511976a06dd723510a2
Eduaney <skiksplinter@hotmail.com>
2022-01-15 12:08:03 -0300

    Show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,13 +1,12 @@
 import MovieScore from 'components/MovieScore';
 import { Link } from 'react-router-dom';
-import { Movie } from "types/movie";
+import { Movie } from 'types/movie';
 
 type Props = {
   movie: Movie;
-}
+};
 
-function MovieCard({ movie } : Props) {
-  
+function MovieCard({ movie }: Props) {
   return (
     <div>
       <img
@@ -17,11 +16,11 @@ function MovieCard({ movie } : Props) {
       />
       <div className="dsmovie-card-bottom-container">
         <h3>{movie.title}</h3>
-        <MovieScore />
+        <MovieScore count={movie.count} score={movie.score} />
 
         <Link to={`/form/${movie.id}`}>
           <div className="btn btn-primary dsmovie-btn">Avaliar</div>
-        </Link> 
+        </Link>
       </div>
     </div>
   );
